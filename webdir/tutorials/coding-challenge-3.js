f8c2afcd2abe352a25123454afb3c0400b555443
Jason Lynch <jlynch9334@gmail.com>
2018-09-15 00:22:00 -0500

    9/14/2018 - Finished Coding Challenge 3

--- webdir/tutorials/coding-challenge-3.js
+++ webdir/tutorials/coding-challenge-3.js
@@ -6,7 +6,9 @@
 /*
 John and his family went on a holiday and went to 3 different resturants. The bills were $123, $48, and $268.
 
-To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less that $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). 
+He likes to tip 20% of the bill when the bill is less that $50, 15% when the bill is between $50 and $200, 
+and 10% if the bill is more than $200.
 
 In the end, John would like to have 2 arrays:
 1) Containing all three tips (one for each bill)
@@ -17,32 +19,60 @@ In the end, John would like to have 2 arrays:
 GOOD LUCK :)
 */
 
+/**
+ * This function will calculate & return the tax for the given currency amount
+ * @param {integer} amount 
+ * @returns amount 
+ */
 function tipCalc(amount) {
 	var totalTip = 0;
-    console.log('amount: ' + amount);
-    
-	if (amount >= 50 || amount <= 200){
-		console.log('condition is true');
-	} else {
-		console.log('condition is false');
-    }
-    
-	switch(amount) {
-		case 'amount < 50':
-			totalTip = (amount * 20) / 100;   
-			return totalTip;
-		case 'amount >= 50 || amount <= 200':
-			totalTip = (amount * 15) / 100;  
-			return totalTip;
-		case 'amount > 200':
-			totalTip = (amount * 10) / 100;  
-			return totalTip;
+	switch(true) {
+		case amount < 50:
+			totalTip = amount * (20 / 100);   
+			return Math.round(totalTip);
+		case amount >= 50 || amount <= 200:
+			totalTip = amount * (15 / 100);  
+			return Math.round(totalTip);
+		case amount > 200:
+			totalTip = amount * (10 / 100);  
+			return Math.round(totalTip);
 		default:
 			return console.log('Error: could not calculate amount');
-    }
+	}
 }
 
-//var bill = [123, 48, 268];
-var bill = 123;
-var totalBill = tipCalc(bill);
-console.log("bill: " + bill);
+
+/* Varible Decleration */
+var bill1 = 123;
+var tip1 = tipCalc(bill1);
+var totalPayment1 = bill1 + tip1;
+
+/* Output */
+console.log("Bill #1: $" + bill1 + " Tip: $" + tip1);
+
+/* Varible Decleration */
+var bill2 = 48;
+var tip2 = tipCalc(bill2);
+var totalPayment2 = bill2 + tip2;
+
+/* Output */
+console.log("Bill #2: $" + bill2 + " Tip: $" + tip2);
+
+/* Varible Decleration */
+var bill3 = 268;
+var tip3 = tipCalc(bill3);
+var totalPayment3 = bill3 + tip3;
+
+/* Output */
+console.log("Bill #3: $" + bill3 + " Tip: $" + tip3);
+
+/* Varible Decleration */
+var allTips = [tip1, tip2, tip3];
+var allPayments = [totalPayment1, totalPayment2, totalPayment3];
+
+/* Output */
+console.log('All Tips: ' + allTips);
+console.log('All Payments: ' + allPayments);
+
+
+
